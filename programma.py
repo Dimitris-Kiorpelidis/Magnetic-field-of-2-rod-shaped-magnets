e541afa8a38b4ce35e9dcef902d5ce7fbd59a220
OriginalKrepas <jjimmys1010@gmail.com>
2025-05-16 23:07:21 +0300

    Made it show both magnets magnetism

--- programma.py
+++ programma.py
@@ -2,35 +2,49 @@ import magpylib as magpy
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Δημιουργία ενός απλού style για την εμφάνιση των μαγνητών, με τη βοήθεια παραδειγμάτων από το user_guide
-#https://magpylib.readthedocs.io/en/latest/_pages/user_guide/examples/examples_vis_magnet_colors.html
+fig, ax = plt.subplots()
 
-mstyle = dict(
-    mode = "color+arrow",
-    color = dict(north = "red", south = "blue"),
-    arrow = dict(width = 2, color = "k")
-)
+#Δημιουργία μαγνητών
+rod_shaped_magnet_1 = magpy.magnet.Cuboid(
 
-
-rod_shaped_magnet_1 = magpy.magnet.Cylinder(
-
-    polarization = (0, 0, 1),
-    dimension = (0.1, 0.2),
-    position = (0.3, 0, 0),
-    style_magnetization = mstyle,                         #Δεν κατάφερα,προς το παρόν, να έχω bicolor στο dictionary mstyle
-    style_magnetization_color_mode = "bicolor"
+    position = (-3, 0, 0),
+    dimension = (1, 0.3, 2.5),
+    polarization = (0, 0, 5000),
 
 )
 
+rod_shaped_magnet_2 = magpy.magnet.Cuboid(
 
-rod_shaped_magnet_2 = magpy.magnet.Cylinder(
-    polarization = (0, 0, 1),
-    dimension = (0.1, 0.2),
-    position = (0.8, 0 ,0),
-    style_magnetization = mstyle,
-    style_magnetization_color_mode = "bicolor"
-    )
+    position = (3, 0, 0),
+    dimension = (1, 0.3, 2.5),
+    polarization = (0, 0, 5000),
+)
+
+magnets = magpy.Collection(rod_shaped_magnet_1, rod_shaped_magnet_2)
 
 
+ts = np.linspace(-5, 5, 40)
+grid = np.array([[(x, 0, z) for x in ts] for z in ts])
+X, _, Z = np.moveaxis(grid, 2, 0)
 
-magpy.show(rod_shaped_magnet_1,rod_shaped_magnet_2)
+
+B = magnets.getB(grid)
+Bx, _, Bz = np.moveaxis(B, 2, 0)
+log10_norm_B = np.log10(np.linalg.norm(B, axis = 2))
+
+
+splt = ax.streamplot(X, Z, Bx, Bz,
+                     density = 1.5,
+                     color = log10_norm_B,
+                     linewidth = log10_norm_B,
+                     cmap = "autumn",
+                     )
+
+
+ax.set(
+    xlabel="x-position (mm)",
+    ylabel="z-position (mm)",
+)
+
+plt.tight_layout()
+plt.show()
