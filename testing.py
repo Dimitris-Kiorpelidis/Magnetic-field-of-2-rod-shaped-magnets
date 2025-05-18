2e160345f4cdeb62c165c9d09f654ea8a2cdbf36
OriginalKrepas <jjimmys1010@gmail.com>
2025-05-18 14:03:36 +0300

    tsting.py finally works

--- testing.py
+++ testing.py
@@ -1,24 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
-
 import magpylib as magpy
 
 # Create a Matplotlib figure
 fig, ax = plt.subplots()
 
 # Create an observer grid in the xy-symmetry plane - using pure numpy
-grid = np.mgrid[-.05:.05:100j, -.05:.05:100j, 0:0:1j].T[0]
+grid = np.mgrid[-5:5:100j, -5:5:100j, 0:0:1j].T[0]
 X, Y, _ = np.moveaxis(grid, 2, 0)
 
-# Compute magnetic field on grid - using the functional interface
-B = magpy.getB(
-    "Cuboid",
-    position = (0,0.01,0.01),
-    observers=grid.reshape(-1, 3),
-    dimension=(0.02, 0.03, 0.05,),
-    polarization=(550,0, 0),
+
+rod_shaped_magnet_1 = magpy.magnet.Cuboid(
+
+    position = (2, 0, 0),
+    dimension = (1.5, 3, 0.5),
+    polarization = (50, 0, 0),
 )
-B = B.reshape(grid.shape)
+
+rod_shaped_magnet_2 = magpy.magnet.Cuboid(
+
+    position = (-2, 0, 0),
+    dimension = (1.5, 3, 0.5),
+    polarization = (50, 0, 0),
+)
+
+magnets = rod_shaped_magnet_1 + rod_shaped_magnet_2
+
+# Compute magnetic field on grid - using the functional interface
+B = magnets.getB(grid)
+# B = B.reshape(grid.shape)
 Bx, By, _ = np.moveaxis(B, 2, 0)
 normB = np.linalg.norm(B, axis=2)
 
