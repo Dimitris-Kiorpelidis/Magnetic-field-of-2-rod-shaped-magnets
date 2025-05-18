a0e4640cb07df8ebf335a1b00be9ee68d7bd37bd
OriginalKrepas <jjimmys1010@gmail.com>
2025-05-18 17:33:38 +0300

    Dynamic x/y and outlines
    Έβαλα μεταβλητές στα x και y των μαγνητών και έκανα το outline τους

--- programma.py
+++ programma.py
@@ -10,41 +10,62 @@ grid = np.mgrid[-5:5:100j, -5:5:100j, 0:0:1j].T[0]
 X, Y, _ = np.moveaxis(grid, 2, 0)
 
 
+x_1 = 2
+x_2 = -2
+y_1 = 0
+y_2 = 0
+
 rod_shaped_magnet_1 = magpy.magnet.Cuboid(
 
-    position = (2, 0, 0),
+    position = (x_1, y_1, 0),
     dimension = (1.5, 3, 0.5),
-    polarization = (50, 0, 0),
+    polarization = (-50, 0, 0),
 )
 
 rod_shaped_magnet_2 = magpy.magnet.Cuboid(
 
-    position = (-2, 0, 0),
+    position = (x_2, y_2, 0),
     dimension = (1.5, 3, 0.5),
     polarization = (50, 0, 0),
 )
+
+
 
 magnets = rod_shaped_magnet_1 + rod_shaped_magnet_2
 
 # Compute magnetic field on grid - using the functional interface
 B = magnets.getB(grid)
-# B = B.reshape(grid.shape)
 Bx, By, _ = np.moveaxis(B, 2, 0)
 normB = np.linalg.norm(B, axis=2)
 
-# Combine streamplot with contourf
-cp = ax.contourf(X, Y, normB, cmap="rainbow", levels=100)
 splt = ax.streamplot(X, Y, Bx, By, color="k", density=1.5, linewidth=1)
 
-# Add colorbar
-fig.colorbar(cp, ax=ax, label="|B| (T)")
+#outline toy prwtoy magnhth
+x_sin1 = rod_shaped_magnet_1.position[0] + rod_shaped_magnet_1.dimension[0]/2
+x_plin1 = rod_shaped_magnet_1.position[0] - rod_shaped_magnet_1.dimension[0]/2
+y_sin1 = rod_shaped_magnet_1.position[1] + rod_shaped_magnet_1.dimension[1]/2
+y_plin1 = rod_shaped_magnet_1.position[1] - rod_shaped_magnet_1.dimension[1]/2
+
+#outline toy deyteroy magnhth
+x_sin2 = rod_shaped_magnet_2.position[0] + rod_shaped_magnet_1.dimension[0]/2
+x_plin2 = rod_shaped_magnet_2.position[0] - rod_shaped_magnet_1.dimension[0]/2
+y_sin2 = rod_shaped_magnet_2.position[1] + rod_shaped_magnet_1.dimension[1]/2
+y_plin2 = rod_shaped_magnet_2.position[1] - rod_shaped_magnet_1.dimension[1]/2
 
 
+ax.plot(
+    [x_sin1, x_sin1, x_plin1, x_plin1, x_sin1],
+    [y_sin1, y_plin1, y_plin1, y_sin1, y_sin1],
+
+    [x_sin2, x_sin2, x_plin2, x_plin2, x_sin2],
+    [y_sin2, y_plin2, y_plin2, y_sin2, y_sin2],
+    lw = 2
+)
 
 # Figure styling
 ax.set(
     xlabel="x-position (m)",
-    ylabel="z-position (m)",
+    ylabel="y-position (m)",
     aspect=1,
 )
 
